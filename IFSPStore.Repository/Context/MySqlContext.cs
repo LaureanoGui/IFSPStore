ff21c482b694160d9aadeff5d9bc66c24867d472
Guilherme Laureano <guitlaureano@gmail.com>
2024-11-05 09:11:43 -0300

    Alterações aula 05-11 (Parcialmente)

---
+++ IFSPStore.Repository/Context/MySqlContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IFSPStore.Domain.Entities;
+
+namespace IFSPStore.Repository.Context
+{
+    public class MySqlContext : DbContext
+    {
+        public MySqlContext(DbContextOptions<MySqlContext> options) :
+        base(options)
+        {
+            Database.EnsureCreated();
+        }
+    
+    public DbSet<Usuario> Usuarios { get; set; }
+    public DbSet<Cidade> Cidades { get; set; }
+    public DbSet<Cliente> Clientes { get; set; }
+    public DbSet<Grupo> Grupos { get; set; }
+    public DbSet<Produto> Produtos { get; set; }
+
+    public DbSet<Venda> Vendas { get; set; }
+    public DbSet<VendaItem> VendaItens { get; set; }
+}
+}
---
+++ IFSPStore.Repository/Mapping/GrupoMap.cs
@@ -0,0 +1,20 @@
+﻿using IFSPStore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace IFSPStore.Repository.Mapping
+{
+    public class GrupoMap : IEntityTypeConfiguration<Grupo>
+    {
+        public void Configure(EntityTypeBuilder<Grupo> builder)
+        {
+            builder.ToTable("Abobrinha");
+            builder.HasKey(prop => prop.Id);
+
+            builder.Property(prop => prop.Nome)
+                .HasColumnName("melancia")
+                .IsRequired()
+                .HasMaxLength(50);
+        }
+    }
+    }
